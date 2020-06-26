5b1672f4f4b856392ab0acc76a7283f8f22928a7
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-06-26 11:32:53 +0700

    go-templates

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hoangnvhybrid/go-templates-example
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type (
+	Todo struct {
+		Title string
+		Done  bool
+	}
+	TodoPageData struct {
+		PageTitle string
+		Todos     []Todo
+	}
+)
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: false},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":9996", nil)
+}
